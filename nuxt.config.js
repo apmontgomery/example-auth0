d24fbb009b32cbbe68c5dcf3af83ed597eb63f91
Alex Montgomery <Alexpm@gmail.com>
2017-12-18 00:13:33 +0800

    commited

--- nuxt.config.js
+++ nuxt.config.js
@@ -25,12 +25,5 @@ module.exports = {
   css: [
     'normalize.css',
     '~assets/main.css'
-  ],
-  /*
-  ** Environement variables
-  */
-  env: {
-    AUTH0_CLIENT_ID: '',
-    AUTH0_CLIENT_DOMAIN: ''
-  }
+  ]
 }
--- utils/lock.js
+++ utils/lock.js
@@ -3,9 +3,9 @@ import { setSecret } from './auth'
 import uuid from 'uuid'
 
 const getLock = (options) => {
-  const config = require('~/config.json')
+  // const config = require('~/config.json')
   const Auth0Lock = require('auth0-lock').default
-  return new Auth0Lock(config.AUTH0_CLIENT_ID, config.AUTH0_CLIENT_DOMAIN, options)
+  return new Auth0Lock(process.env.AUTH0_CLIENT_ID, process.env.AUTH0_CLIENT_DOMAIN, options)
 }
 
 const getBaseUrl = () => `${window.location.protocol}//${window.location.host}`
